74522c4c3b8e577f93d7786e458a3b73c2f232b3
Kourtney Smith <klsmith5370@gmail.com>
2025-10-07 19:00:06 -0400

    Updated logout button with loading status, working on setting up functionality of log out when clicked

--- note-master/src/components/LogoutButton.tsx
+++ note-master/src/components/LogoutButton.tsx
@@ -1,5 +1,16 @@
+"use client";
+
+import { Loader2 } from "lucide-react";
+import { Button } from "./ui/button";
+import { useState } from "react";
+
 function LogoutButton() {
-    return <div>LogoutButton</div>
+    const [loading, setLoading] = useState(false);
+    return (
+        <Button className="w-24" variant="outline" onClick={handleLogOut}>
+            {loading ? <Loader2 className="animate-spin" /> : "Logout"}
+        </Button>
+    );
 }
 
 export default LogoutButton;

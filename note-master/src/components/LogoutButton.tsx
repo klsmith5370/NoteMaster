232896cfaa197c8345306da6da23199dbd7855b5
Kourtney Smith <klsmith5370@gmail.com>
2025-10-08 19:21:53 -0400

    Added onclick function for logging out

--- note-master/src/components/LogoutButton.tsx
+++ note-master/src/components/LogoutButton.tsx
@@ -6,6 +6,11 @@ import { useState } from "react";
 
 function LogoutButton() {
     const [loading, setLoading] = useState(false);
+
+    const handleLogOut = () => {
+        console.log("Logging out...");
+    };
+
     return (
         <Button className="w-24" variant="outline" onClick={handleLogOut}>
             {loading ? <Loader2 className="animate-spin" /> : "Logout"}

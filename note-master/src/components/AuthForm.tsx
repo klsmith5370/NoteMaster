de8c79228cd5f19db76d5be1959a8fe489ef53ef
Kourtney Smith <klsmith5370@gmail.com>
2025-11-05 18:32:51 -0500

    Added form section for password entry

--- note-master/src/components/AuthForm.tsx
+++ note-master/src/components/AuthForm.tsx
@@ -1,10 +1,11 @@
 "use client"
 
 import { useRouter } from "next/navigation";
-import { CardContent } from "./ui/card";
+import { CardContent, CardFooter } from "./ui/card";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { useTransition } from "react";
+import { Button } from "./ui/button";
 
 type Props = {
     type: "login" | "signUp";
@@ -15,16 +16,17 @@ function AuthForm({type}: Props) {
     const router = useRouter();
     // const { toast } = useToast();
 
-    const [isPending, startTransaction] = useTransition()
+    const [isPending, startTransaction] = useTransition();
 
     const handleSubmit = (FormData: FormData) => {
         console.log("form submitted")
     }
 
     return <form action={handleSubmit}>
-        <CardContent>
-            <div>
+        <CardContent className="grid w-full items-center gap-4">
+            <div className= "flex flex-col space-y-1.5">
                 <Label htmlFor="email">Email</Label>
+                
                 <Input 
                 id="email"
                 name="email"
@@ -34,7 +36,22 @@ function AuthForm({type}: Props) {
                 disabled={isPending}
                 />
             </div>
+            <div className= "flex flex-col space-y-1.5">
+                <Label htmlFor="email">Password</Label>
+                
+                <Input 
+                id="password"
+                name="password"
+                placeholder="Enter your password"
+                type="password"
+                required
+                disabled={isPending}
+                />
+            </div>
         </CardContent>
+        <CardFooter>
+            <Button></Button>
+        </CardFooter>
     </form>
 }
 
